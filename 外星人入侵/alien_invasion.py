97b237593bc48f206ef48017e25dbedadde43743
topmost <123@123.com>
2020-01-02 15:23:42 +0800

    飞船可以发射子弹了

--- 外星人入侵/alien_invasion.py
+++ 外星人入侵/alien_invasion.py
@@ -1,4 +1,5 @@
 import pygame
+from pygame.sprite import Group
 from settings import Settings
 from ship import Ship
 import game_functions as gf 
@@ -11,12 +12,17 @@ def run_game():
     pygame.display.set_caption("Alien Invasion")
 
     #创建一艘飞船
-    ship = Ship(screen)
+    ship = Ship(ai_settings, screen)
+    #创建一个用于存储子弹的编组
+    bullets = Group()
+
     #开始游戏的主循环
     while True:
 
         #监视键盘和鼠标事件
-        gf.check_events()
-        gf.update_screen(ai_settings, screen, ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
+        ship.update()
+        gf.update_bullets(bullets)
+        gf.update_screen(ai_settings, screen, ship, bullets)
 
 run_game()
--- 外星人入侵/settings.py
+++ 外星人入侵/settings.py
@@ -1,5 +1,5 @@
 class Settings():
-    '''存储《外星人入侵所有设置的类》'''
+    '''存储《外星人入侵》的所有设置的类'''
 
     def __init__(self):
         '''初始化游戏的设置'''
@@ -7,3 +7,13 @@ class Settings():
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
+
+        #飞船的设置
+        self.ship_speed_factor = 1.5
+
+        #子弹设置
+        self.bullet_speed_factor = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = 60, 60, 60
+        self.bullet_allowed = 3
