de1847309664dfd54ebfeeead58f0d33104c791b
topmost <123@123.com>
2020-01-03 10:43:47 +0800

    外星人可以移动了

--- 外星人入侵/alien_invasion.py
+++ 外星人入侵/alien_invasion.py
@@ -2,7 +2,6 @@ import pygame
 from pygame.sprite import Group
 from settings import Settings
 from ship import Ship
-from alien import Alien
 import game_functions as gf 
 
 def run_game():
@@ -12,12 +11,13 @@ def run_game():
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
-    #创建一艘飞船
+    #创建一艘飞船, 一个子弹编组和一个外星人编组
     ship = Ship(ai_settings, screen)
-    #创建一个用于存储子弹的编组
     bullets = Group()
-    #创建一个外星人
-    alien = Alien(ai_settings, screen)
+    aliens = Group()
+   
+    #创建外星人群
+    gf.create_fleet(ai_settings, screen, ship, aliens)
 
     #开始游戏的主循环
     while True:
@@ -26,6 +26,7 @@ def run_game():
         gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
         gf.update_bullets(bullets)
-        gf.update_screen(ai_settings, screen, ship, alien, bullets)
+        gf.update_aliens(ai_settings, aliens)
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 
 run_game()
--- 外星人入侵/settings.py
+++ 外星人入侵/settings.py
@@ -17,3 +17,9 @@ class Settings():
         self.bullet_height = 15
         self.bullet_color = 60, 60, 60
         self.bullet_allowed = 3
+
+        #外星人设置
+        self.alien_speed_factor = 1
+        self.fleet_drop_speed = 10
+        # fleet_direction = 1表示向右移，为-1表示向左移
+        self.fleet_direction = 1
